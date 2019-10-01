d637a569585ba59e4c98f86d6912cf16cb5792c3
Shivendra Saurav <shivendrasaurav@gmail.com>
2019-10-02 01:48:45 +0530

    Service Worker Working

---
+++ sw.js
@@ -0,0 +1,18 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/fluent.css',
+       '/calc.js',
+       '/fluent.js'
+     ]);
+   })
+ );
+});
